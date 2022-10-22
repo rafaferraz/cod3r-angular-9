e9f071fb27a69c4c5dc8f95628e7ab108a88c948
Rafaela Cristina Ferraz <rafaelacristina@gec.inatel.br>
2022-10-22 13:57:46 -0300

    feat: create footer component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { HeaderComponent } from './components/template/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        HeaderComponent
+        HeaderComponent,
+        FooterComponent
     ],
     imports: [
         BrowserModule,
