1eb48e13058ba0c011a89344c247871fc610f17b
Rafaela Cristina Ferraz <rafaelacristina@gec.inatel.br>
2022-10-25 18:18:34 -0300

    feat: create product service and createProduct and cancel functions

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
@@ -35,7 +36,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
         MatSidenavModule,
         MatCardModule,
         MatListModule,
-        MatButtonModule
+        MatButtonModule,
+        MatSnackBarModule
     ],
     providers: [],
     bootstrap: [AppComponent]
