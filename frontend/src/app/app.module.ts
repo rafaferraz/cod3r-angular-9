e86225d1272f6867f497f0c30a36be7ae2977b6c
Rafaela Cristina Ferraz <rafaelacristina@gec.inatel.br>
2022-10-26 18:50:01 -0300

    feat: implement products table component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
     declarations: [
@@ -47,7 +48,8 @@ import { ProductReadComponent } from './components/product/product-read/product-
         HttpClientModule,
         FormsModule,
         MatFormFieldModule,
-        MatInputModule
+        MatInputModule,
+        MatTableModule
     ],
     providers: [],
     bootstrap: [AppComponent]
