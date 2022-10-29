80d8a26a6350b0a549edccf344487ebdd687ab17
Rafaela Cristina Ferraz <rafaelacristina@gec.inatel.br>
2022-10-29 12:33:42 -0300

    feat: add delete product component and methods

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -14,7 +14,7 @@ export class ProductCreateComponent implements OnInit {
 
   product: Product = {
     name: '',
-    price: null
+    price: 0
   }
 
   ngOnInit(): void { }
---
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.scss']
+})
+export class ProductDeleteComponent implements OnInit {
+
+  product!: Product
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productService.readById(id as string).subscribe((product) => {
+      this.product = product;
+    });
+  }
+
+  deleteProduct(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productService.delete(id as string).subscribe(() => {
+      this.productService.showMessage("Produto excluido com sucesso!");
+      this.router.navigate(["/products"]);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+}
